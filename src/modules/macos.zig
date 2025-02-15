1e645750426ea5862218ee708f764236a730f72c
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-02-14 22:20:11 -0500

    rather difficult.

--- src/modules/macos.zig
+++ src/modules/macos.zig
@@ -4,27 +4,42 @@ pub const libSystem = struct {
 
 pub const libobjc = struct {
     pub const classes = struct {
+        pub var NSObject: objc_Class = undefined;
         pub var NSString: objc_Class = undefined;
         pub var NSApplication: objc_Class = undefined;
         pub var NSWindow: objc_Class = undefined;
     };
     pub const selectors = struct {
         pub var alloc: objc_SEL = undefined;
+        pub var release: objc_SEL = undefined;
         pub var run: objc_SEL = undefined;
+        pub var @"stop:": objc_SEL = undefined;
         pub var sharedApplication: objc_SEL = undefined;
         pub var @"setTitle:": objc_SEL = undefined;
         pub var @"setActivationPolicy:": objc_SEL = undefined;
         pub var @"initWithContentRect:styleMask:backing:defer:": objc_SEL = undefined;
         pub var @"makeKeyAndOrderFront:": objc_SEL = undefined;
         pub var @"initWithUTF8String:": objc_SEL = undefined;
+        pub var @"windowWillClose:": objc_SEL = undefined;
+        pub var @"setDelegate:": objc_SEL = undefined;
+        pub var @"applicationDidFinishLaunching:": objc_SEL = undefined;
+        pub var @"nextEventMatchingMask:untilDate:inMode:dequeue:": objc_SEL = undefined;
+        pub var @"sendEvent:": objc_SEL = undefined;
     };
 
     pub const objc_Class = *opaque {};
     pub const objc_SEL = *opaque {};
+    pub const objc_Protocol = opaque {};
 
     pub extern "objc" fn objc_msgSend() void;
     pub extern "objc" fn objc_getClass(name: ?[*:0]const u8) ?objc_Class;
     pub extern "objc" fn sel_getUid(name: ?[*:0]const u8) ?objc_SEL;
+    pub extern "objc" fn objc_allocateClassPair(superclass: ?objc_Class, name: ?[*:0]const u8, extraBytes: usize) ?objc_Class;
+    pub extern "objc" fn objc_registerClassPair(cls: ?objc_Class) void;
+    pub extern "objc" fn class_addProtocol(cls: ?objc_Class, protocol: ?*objc_Protocol) c_int;
+    pub extern "objc" fn objc_getProtocol(name: ?[*:0]const u8) ?*objc_Protocol;
+    pub extern "objc" fn class_addMethod(cls: ?objc_Class, name: ?objc_SEL, imp: ?*const fn () callconv(.c) void, types: ?[*:0]const u8) c_int;
+    pub extern "objc" fn class_createInstance(cls: ?objc_Class, extraBytes: usize) ?*anyopaque;
 };
 
 pub const fwCoreFoundation = struct {
@@ -56,6 +71,18 @@ pub const fwFoundation = struct {
 };
 
 pub const fwAppKit = struct {
+    pub extern const NSDefaultRunLoopMode: ?*anyopaque;
+
+    pub const NSEvent = opaque {
+        pub const EventMask = enum(c_ulonglong) {
+            any = 0xFFFFFFFF,
+        };
+
+        pub fn release(self: *NSEvent) void {
+            return @as(*const fn (cls: ?*NSEvent, sel: ?libobjc.objc_SEL) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.release);
+        }
+    };
+
     pub const NSApplication = opaque {
         pub const ActivationPolicy = enum(c_int) {
             regular = 0,
@@ -73,6 +100,22 @@ pub const fwAppKit = struct {
 
         pub fn run(self: *NSApplication) void {
             return @as(*const fn (obj: ?*NSApplication, sel: ?libobjc.objc_SEL) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.run);
+        }
+
+        pub fn @"stop:"(self: *NSApplication, sender: ?*anyopaque) void {
+            return @as(*const fn (obj: ?*NSApplication, sel: ?libobjc.objc_SEL, sender: ?*anyopaque) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.@"stop:", sender);
+        }
+
+        pub fn @"setDelegate:"(self: *NSApplication, delegate: *anyopaque) void {
+            return @as(*const fn (obj: ?*NSApplication, sel: ?libobjc.objc_SEL, delegate: ?*anyopaque) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.@"setDelegate:", delegate);
+        }
+
+        pub fn @"nextEventMatchingMask:untilDate:inMode:dequeue:"(self: *NSApplication, mask: NSEvent.EventMask, untilDate: ?*anyopaque, inMode: ?*anyopaque, dequeue: c_int) ?*NSEvent {
+            return @as(*const fn (obj: ?*NSApplication, sel: ?libobjc.objc_SEL, mask: NSEvent.EventMask, untilDate: ?*anyopaque, inMode: ?*anyopaque, dequeue: c_int) callconv(.c) ?*NSEvent, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.@"nextEventMatchingMask:untilDate:inMode:dequeue:", mask, untilDate, inMode, dequeue);
+        }
+
+        pub fn @"sendEvent:"(self: *NSApplication, event: *NSEvent) void {
+            return @as(*const fn (obj: ?*NSApplication, sel: ?libobjc.objc_SEL, event: ?*NSEvent) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.@"sendEvent:", event);
         }
     };
 
@@ -104,6 +147,10 @@ pub const fwAppKit = struct {
 
         pub fn @"makeKeyAndOrderFront:"(self: *NSWindow, sender: ?*anyopaque) void {
             return @as(*const fn (obj: ?*NSWindow, sel: ?libobjc.objc_SEL, sender: ?*anyopaque) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.@"makeKeyAndOrderFront:", sender);
+        }
+
+        pub fn @"setDelegate:"(self: *NSWindow, delegate: *anyopaque) void {
+            return @as(*const fn (obj: ?*NSWindow, sel: ?libobjc.objc_SEL, delegate: ?*anyopaque) callconv(.c) void, @ptrCast(&libobjc.objc_msgSend))(self, libobjc.selectors.@"setDelegate:", delegate);
         }
     };
 };
--- src/platform/main_macos.zig
+++ src/platform/main_macos.zig
@@ -9,7 +9,20 @@ pub const m = struct {
     pub usingnamespace macos.fwAppKit;
 };
 
-pub const platform = struct {};
+pub const platform = struct {
+    var app: *m.NSApplication = undefined;
+    var running = true;
+};
+
+fn AsunderApplicationDelegate_applicationDidFinishLaunching(self: ?*anyopaque, sel: ?*anyopaque, notification: ?*anyopaque) callconv(.c) void {
+    _ = .{ self, sel, notification };
+    platform.app.@"stop:"(null);
+}
+
+fn AsunderWindowDelegate_windowWillClose(self: ?*anyopaque, sel: ?*anyopaque, notification: ?*anyopaque) callconv(.c) void {
+    _ = .{ self, sel, notification };
+    platform.running = false;
+}
 
 fn entry() void {
     inline for (@typeInfo(m.libobjc.classes).@"struct".decls) |decl| {
@@ -19,8 +32,20 @@ fn entry() void {
         @field(m.libobjc.selectors, decl.name) = m.sel_getUid(decl.name).?;
     }
 
-    const app = m.NSApplication.sharedApplication().?;
-    _ = app.@"setActivationPolicy:"(.regular);
+    const AsunderApplicationDelegate = m.objc_allocateClassPair(m.classes.NSObject, "AsunderApplicationDelegate", 0).?;
+    _ = m.class_addMethod(AsunderApplicationDelegate, m.selectors.@"applicationDidFinishLaunching:", @ptrCast(&AsunderApplicationDelegate_applicationDidFinishLaunching), "v@:@");
+    m.objc_registerClassPair(AsunderApplicationDelegate);
+
+    const AsunderWindowDelegate = m.objc_allocateClassPair(m.classes.NSObject, "AsunderWindowDelegate", 0).?;
+    _ = m.class_addProtocol(AsunderWindowDelegate, m.objc_getProtocol("NSWindowDelegate").?);
+    _ = m.class_addMethod(AsunderWindowDelegate, m.selectors.@"windowWillClose:", @ptrCast(&AsunderWindowDelegate_windowWillClose), "v@:@");
+    m.objc_registerClassPair(AsunderWindowDelegate);
+
+    platform.app = m.NSApplication.sharedApplication().?;
+    _ = platform.app.@"setActivationPolicy:"(.regular);
+
+    const application_delegate = m.class_createInstance(AsunderApplicationDelegate, 0).?;
+    platform.app.@"setDelegate:"(application_delegate);
 
     const window = m.NSWindow.alloc().?.@"initWithContentRect:styleMask:backing:defer:"(
         .{ .origin = .{ .x = 0, .y = 0 }, .size = .{ .w = 600, .h = 400 } },
@@ -29,9 +54,22 @@ fn entry() void {
         false,
     ).?;
     window.@"setTitle:"(m.NSString.alloc().?.@"initWithUTF8String:"("Asunder").?);
+
+    const window_delegate = m.class_createInstance(AsunderWindowDelegate, 0).?;
+    window.@"setDelegate:"(window_delegate);
+
     window.@"makeKeyAndOrderFront:"(null);
 
-    app.run();
+    platform.app.run();
+
+    while (platform.running) {
+        while (true) {
+            const event = platform.app.@"nextEventMatchingMask:untilDate:inMode:dequeue:"(.any, null, m.NSDefaultRunLoopMode, @intFromBool(true));
+            if (event == null) break;
+            platform.app.@"sendEvent:"(event.?);
+            event.?.release();
+        }
+    }
 }
 
 pub export fn _start() noreturn {
