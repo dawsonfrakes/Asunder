d6b39025db3ed60077901e97ec1b2f8fc9715541
Dawson Frakes <dawsonmfrakes@gmail.com>
2025-02-22 20:10:29 -0500

    just do the thing man i guess

--- src/main.zig
+++
@@ -1,6 +0,0 @@
-const basic = @import("basic/basic.zig");
-
-pub usingnamespace switch (basic.os_tag) {
-	.windows => @import("platform/main_windows.zig"),
-	else => |tag| @compileLog("OS {s} not supported", .{@tagName(tag)}),
-};
